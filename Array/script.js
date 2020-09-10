a4d89b257da45e86f209289741317c59fdedf55f
RitiArya <Riti.Arya@telusinternational.com>
2020-09-10 21:34:02 +0530

    Uploading Script task files

---
+++ Array/script.js
@@ -0,0 +1,15 @@
+var person = [
+    {name : "Riya"},
+    {name : "Reshu"},
+    {name : "Tina"},
+    {name : "Puja"},
+    {name : "Ram"},
+    {name : "Shyam"},
+    {name : "Mysture"},
+    {name : "Paras"},
+    {name : "Tanuj"},
+    {name : "Priya"}
+       ];
+for(var a= 0; a<person.length; a++){
+  document.write(person[a].name + "<br>");
+}
