62a603b36fecdf61ef740a533e2fbdaead65ef67
RitiArya <Riti.Arya@telusinternational.com>
2020-09-17 21:21:22 +0530

    Uploading update Login task files

--- SignInForm/login.js
+++ SignInForm/login.js
@@ -1,24 +1,25 @@
-function Validate() {
-    var Username = document.getElementById('Username');
-    var Password = document.getElementById('Password');
-    if (Username.value=="" && Password.value=="" )
-    {
-       alert("Kindly fill all details");
-      return true;
-    }
-    else if(Username.value=="")
-    {
-      alert("Please enter Username");
-      return false;
-    }
-    else if(Password.value=="")
-    {
-      alert("Please enter Password");
-      return false;
-    }
-    else 
-    {
-        alert("Login Successfully");
-        return true;
-    }
+function validateForm() {
+  var username = document.getElementById('username');
+  var password = document.getElementById('password');
+  if (username.value=="" && password.value=="" )
+  {
+     alert("Kindly fill all details");
+  }
+  else if(username.value=="")
+  {
+    alert("Please enter Username");
+  }
+  else if(password.value=="")
+  {
+    alert("Please enter Password");
+  }
+  else 
+  {
+    var span = document.getElementById('name');
+    span.innerText = username.value;
+    var form1 = document.getElementById('form1');
+    form1.style.display='';  
+    var message = document.getElementById('message');
+    message.style.display='none';
+  }
 }
